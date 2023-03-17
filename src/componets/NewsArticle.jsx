c40b1c966f3da98b60aed9e70f5da32aaf2ab475
Pheogrammer <pheogrammer@github.com>
2023-03-17 19:46:44 +0300

    Added a text length limiter for articled headings and descrption so thatthey wont take much space

--- src/componets/NewsArticle.jsx
+++ src/componets/NewsArticle.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import './NewsArticles.css';
 
-function NewsArticle( {article} ) {
+function NewsArticle({ article }) {
   return (
     <div className='news-card' >
-        <div>
-            <img className='news-img' src={article.urlToImage} alt={article.title} />
-        </div>
-        <div className="card-content">
-          <h2> {article.title} </h2>
-          <p className=''>{article.description}</p>
-        </div>
-        <a href={article.url} target="_blank">Read More</a>
+      <div>
+        <img className='news-img' src={article.urlToImage} alt={article.title} />
+      </div>
+      <div className="card-content">
+        <h2>{article.title.length > 80 ?
+          `${article.title.substring(0, 80)}...` : article.title
+        }  </h2>
+        <p className=''>{article.description.length > 200 ?
+          `${article.description.substring(0, 200)}...` : article.description
+        }</p>
+      </div>
+      <a href={article.url} target="_blank">Read More</a>
     </div>
   )
 }
