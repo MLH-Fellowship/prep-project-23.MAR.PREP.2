e2cbbcfcb5a0586e6d979c3d4daf3b9507f93cf8
SandyCreek <xiezhuoli771@gmail.com>
2023-03-20 17:18:36 -0500

    added button styling

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   const [buttonClick, setButtonClick] = useState(false);
 
   useEffect(() => {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + process.env.REACT_APP_APIKEY)
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + "&appid=" + process.env.REACT_APP_APIKEY)
       .then(res => res.json())
       .then(
         (result) => {
