f9fde96f2ab25dfdefe5297cd1a9af3cdf0902d0
Polu Tharun Kumar Reddy <polutharun528@gmail.com>
2023-03-20 23:17:29 -0400

    Updated as per review comments

--- src/components/SuggestedThings/Box.jsx
+++ src/components/SuggestedThings/Box.jsx
@@ -6,7 +6,7 @@ import suggestedThings from './suggestedThings';
 function Item(props) {
     return (
         <div key={props.name} className="flex suggested-thing">
-            <img className="icons" src={props.img} alt={props.alt} width="50" height="50"></img>
+            <img className="icons" src={props.img} alt={props.alt} width="50" height="50" />
             <h3>{props.name}</h3>
         </div>
     );
@@ -15,13 +15,14 @@ function Item(props) {
 //This function returns the container which holds the three suggested items of a particular weather type.
 function Box(props){
     const weatherType = props.weather.weather[0].main
+    const suggestedThingsKey = suggestedThings[weatherType]
     return (
         <div className="flex suggested-things-box">
-            {Object.keys(suggestedThings[weatherType]).map((item) => {
+            {Object.keys(suggestedThingsKey).map((item) => {
                 return (
                     <Item
-                    img={suggestedThings[weatherType][item]}
-                    alt="Suggested thing images"
+                    img={suggestedThingsKey[item]}
+                    alt={suggestedThingsKey}
                     name={item}
                     />
                 );
