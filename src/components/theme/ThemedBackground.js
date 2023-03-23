8b9d3a7f19b78c23768176e4bcde8b9e30118405
BarokDG <dagimbarok@gmail.com>
2023-03-24 01:06:33 +0300

    Add remaining images

--- src/components/theme/ThemedBackground.js
+++ src/components/theme/ThemedBackground.js
@@ -1,12 +1,13 @@
 import { getBackgroundImageFromWeatherCondition } from "../../lib/getBackgroundImageFromWeatherCondition.js";
 
 export default function ThemedBackground({ weatherCondition }) {
+  const imageSrc = getBackgroundImageFromWeatherCondition(weatherCondition);
+
+  if (!imageSrc) return null;
+
   return (
     <div className="themed-background">
-      <img
-        src={getBackgroundImageFromWeatherCondition(weatherCondition)}
-        alt=""
-      />
+      <img src={imageSrc} alt="" />
 
       {/* This div is used to create a dark overlay on the background image so the rest of the interface is still easy to read */}
       <div className="overlay"></div>
--- src/lib/getBackgroundImageFromWeatherCondition.js
+++ src/lib/getBackgroundImageFromWeatherCondition.js
@@ -1,59 +1,39 @@
+import AshIMG from "../assets/images/ash.jpg";
 import CloudsIMG from "../assets/images/clouds.jpg";
 import ClearIMG from "../assets/images/clear.jpg";
-import ThunderstormIMG from "../assets/images/thunderstorm.jpg";
+import DrizzleIMG from "../assets/images/drizzle.jpg";
+import DustIMG from "../assets/images/dust.jpg";
+import FogIMG from "../assets/images/fog.jpg";
+import HazeIMG from "../assets/images/haze.jpg";
+import MistIMG from "../assets/images/mist.jpg";
 import RainIMG from "../assets/images/rain.jpg";
+import SandIMG from "../assets/images/sand.jpg";
+import SmokeIMG from "../assets/images/smoke.jpg";
 import SnowIMG from "../assets/images/snow.jpg";
+import SquallIMG from "../assets/images/squall.jpg";
+import ThunderstormIMG from "../assets/images/thunderstorm.jpg";
+import TornadoIMG from "../assets/images/tornado.jpg";
 
 const mapWeatherConditionToBackground = {
-  Clouds: {
-    imageSrc: CloudsIMG,
-  },
-  Thunderstorm: {
-    imageSrc: ThunderstormIMG,
-  },
-  Clear: {
-    imageSrc: ClearIMG,
-  },
-  Drizzle: {
-    imageSrc: null,
-  },
-  Rain: {
-    imageSrc: RainIMG,
-  },
-  Snow: {
-    imageSrc: SnowIMG,
-  },
-  Mist: {
-    imageSrc: null,
-  },
-  Smoke: {
-    imageSrc: null,
-  },
-  Haze: {
-    imageSrc: null,
-  },
-  Dust: {
-    imageSrc: null,
-  },
-  Fog: {
-    imageSrc: null,
-  },
-  Sand: {
-    imageSrc: null,
-  },
-  Ash: {
-    imageSrc: null,
-  },
-  Squall: {
-    imageSrc: null,
-  },
-  Tornado: {
-    imageSrc: null,
-  },
+  Ash: AshIMG,
+  Clear: ClearIMG,
+  Clouds: CloudsIMG,
+  Drizzle: DrizzleIMG,
+  Dust: DustIMG,
+  Fog: FogIMG,
+  Haze: HazeIMG,
+  Mist: MistIMG,
+  Rain: RainIMG,
+  Sand: SandIMG,
+  Smoke: SmokeIMG,
+  Snow: SnowIMG,
+  Squall: SquallIMG,
+  Thunderstorm: ThunderstormIMG,
+  Tornado: TornadoIMG,
 };
 
 export function getBackgroundImageFromWeatherCondition(weather) {
   // will show default blue background as fallback for now
 
-  return mapWeatherConditionToBackground[weather].imageSrc ?? "";
+  return mapWeatherConditionToBackground[weather] ?? "";
 }
