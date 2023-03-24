057825110dbe9f1a33c3bcc0f890f44e93ec7220
Polu Tharun Kumar Reddy <polutharun528@gmail.com>
2023-03-24 00:19:07 -0400

    Fixed merge conflicts

---
+++ src/App.js
@@ -0,0 +1,166 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import logo from "./mlh-prep.png";
+import Box from './components/SuggestedThings/Box';
+import AirPollution from "./AirPollution";
+import Sun from "./Sun";
+import MapComponent from "./map";
+import ThemedBackground from "./components/theme/ThemedBackground";
+import WeatherByHourData from "./components/WeatherByHourData";
+import ExampleCustomTimeInput from "./components/ExampleCustomTimeInput";
+import { addDays } from "date-fns";
+import calendarIcon from "./images/55281.png";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+function App(props) {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [city, setCity] = useState("New York City");
+  const [results, setResults] = useState(null);
+  const [searchedLocation, setSearchedLocation] = useState(null);
+  const [date, setDate] = useState(new Date());
+  const arrayOfTimes = [
+    "SelectTime",
+    "00:00:00",
+    "03:00:00",
+    "06:00:00",
+    "09:00:00",
+    "12:00:00",
+    "15:00:00",
+    "18:00:00",
+    "21:00:00",
+  ];
+  const [timeOption, setTimeOption] = useState(arrayOfTimes[0]);
+
+  // This function is called when the user submits the form
+  const handleCitySubmit = (event) => {
+    event.preventDefault();
+    setIsLoaded(false);
+    setResults(null);
+    // Fetch weather data from OpenWeatherMap API
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_APIKEY}`
+    )
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          if (result["cod"] !== 200) {
+            setIsLoaded(false);
+          } else {
+            setIsLoaded(true);
+            setResults(result);
+            setSearchedLocation([result.coord.lat, result.coord.lon]); // Set the coordinates of the searched location
+          }
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  };
+  // If there's an error, log it to the console
+  if (error) {
+    console.error(error);
+  } else {
+    return (
+      <>
+        <div className="fill-height">
+          <img className="logo" src={logo} alt="MLH Prep Logo"></img>
+          <div className="Page">
+            <h2>Enter a city below 👇</h2>
+            <form onSubmit={handleCitySubmit}>
+              <input
+                type="text"
+                value={city}
+                onChange={(event) => setCity(event.target.value)}
+              />
+            </form>
+            <br />
+            <span>
+              <div className="date-picker-div">
+                <DatePicker
+                  selected={date}
+                  onChange={(date) => setDate(date)}
+                  showTimeInput
+                  customTimeInput={
+                    <ExampleCustomTimeInput
+                      timeOption={timeOption}
+                      setTimeOption={setTimeOption}
+                      arrayOfTimes={arrayOfTimes}
+                    />
+                  }
+                  minDate={new Date()}
+                  maxDate={addDays(new Date(), 5)}
+                />
+              </div>
+              <div className="pic-container">
+                <img
+                  src={calendarIcon}
+                  alt="calendar icon"
+                  className="calendarIcon"
+                ></img>
+              </div>
+            </span>
+            <div className="Results">
+              {!isLoaded && <h2>Loading...</h2>}
+              {isLoaded && results && (
+                <>
+                  <h3>{results.weather[0].main}</h3>
+                  <p>Feels like {results.main.feels_like}°C</p>
+                  <Sun
+                    latitude={results.coord.lat}
+                    longitude={results.coord.lon}
+                    timezone={results.timezone}
+                  />
+                  <i>
+                    <p>
+                      {results.name}, {results.sys.country}
+                    </p>
+                  </i>
+                </>
+              )}
+            </div>
+            {results === null ? (
+              <div>
+                <h1>Loading...</h1>
+              </div>
+            ) : (
+              <WeatherByHourData
+                results={results}
+                timeOption={timeOption}
+                date={date}
+              />
+            )}
+          </div>
+          <div className="center">
+            {!isLoaded && <h2>Loading...</h2>}
+            {isLoaded && results && (
+              <AirPollution lat={results.coord.lat} lon={results.coord.lon} />
+            )}
+          </div>
+          <h2 className="suggested-things-heading">Things you need to carry 🎒</h2>
+          {/* Displays the 'Box' component if results(API response) is not null.
+          Here, the API response is passed as props to the Box component*/}
+          {results == null ? 
+          <div>
+            <h2>Loading...</h2>
+          </div> : 
+          <div>
+            <Box weather={results} />
+          </div>}
+          <MapComponent
+            searchedLocation={searchedLocation}
+            searchedLocationName={city}
+          />
+
+          {results && (
+            <ThemedBackground weatherCondition={results?.weather[0].main} />
+          )}
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/WeatherByHourData.js
@@ -0,0 +1,80 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+const WeatherByHourData = (results) => {
+    const [weatherData, setWeatherData] = useState([]);
+    const [count, setCount] = useState(0)
+    const timeOption = results.timeOption;
+    const date = results.date.toISOString().slice(0, 10);
+    const cityName = results.results.name;
+
+    useEffect(() => {
+        axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=metric&appid=${process.env.REACT_APP_APIKEY}`)
+            .then(res => {
+                setWeatherData(res.data.list)
+                setCount(count + 1)
+            })
+            .catch(err => {
+                console.log(err.message)
+            })
+
+
+    }, [cityName, date, timeOption])
+
+    useEffect(() => {
+        checkChildForErrorMessage()
+    }, [])
+
+    const filterDataByDate = (date, timeOption) => {
+
+        if (timeOption === "SelectTime" || timeOption === '') {
+            return <div className='error-message'>
+                <h1>Please select a time and date to get weather information</h1>
+            </div>
+        } 
+
+        return weatherData.filter(data => data.dt_txt === `${date} ${timeOption}`).map(filteredData => {
+            return <div className="Specific-Results">
+                <div className='weather-icon-container'>
+                    <img src={`https://openweathermap.org/img/wn/${filteredData.weather[0].icon}@2x.png`} alt="weather icon" className='weather-icon'/>
+                </div>
+                <table>
+                    <tr>
+                        <th>Temperature</th>
+                        <th>Humidity</th>
+                        <th>Wind Speed</th>
+                        <th>Outside</th>
+                    </tr>
+                    <tr>
+                        <td>{filteredData.main.feels_like}°C</td>
+                        <td>{filteredData.main.humidity}</td>
+                        <td>{filteredData.wind.speed} mph</td>
+                        <td>{filteredData.weather[0].main}</td>
+                    </tr>
+                </table>
+            </div>
+        })
+    }
+
+    // This method checks the dom to ensure that child, which is the name of the 
+    // container that contains the weather data from the forecast api call is null
+    // and if count is above 1, which it will increase whenever after the first api call is made.
+    // This method is important because it will allow for the user to know that the data that they request is not
+    // available due to the limitations of the API. 
+
+    const checkChildForErrorMessage = (child) => {
+        child = document.querySelector(".Specific-Results")
+        if (child === null && count > 1) {
+            return <h1 className='error-message'>Data not available</h1>
+        }
+    }
+
+    return (
+        <div className='component-window'>
+            {filterDataByDate(date, timeOption)}
+            {checkChildForErrorMessage()}
+        </div>
+    )
+}
+
+export default WeatherByHourData
